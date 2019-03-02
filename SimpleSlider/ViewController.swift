ce93f5e00a954f08963580f787d277a2b8ed4574
Ricardo <eehaha@gmail.com>
2019-03-02 12:44:36 +0800

    initial commit

--- SimpleSlider/ViewController.swift
+++ SimpleSlider/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var label: UILabel!
+    @IBAction func sliderChanged(_ sender: Any) {
+        print(slider.value)
+        label.text = "Current Value: \(slider.value)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
